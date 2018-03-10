ba2941c9c2c166c7ff570def02e63296b83341be
mehedi-xion <mehedi.thedue@gmail.com>
2018-03-11 02:07:12 +0600

    starting passport

--- app/Http/Resources/Product/ProductCollection.php
+++ app/Http/Resources/Product/ProductCollection.php
@@ -2,9 +2,9 @@
 
 namespace App\Http\Resources\Product;
 
-use Illuminate\Http\Resources\Json\ResourceCollection;
+use Illuminate\Http\Resources\Json\Resource;
 
-class ProductCollection extends ResourceCollection
+class ProductCollection extends Resource
 {
     /**
      * Transform the resource collection into an array.
@@ -14,6 +14,14 @@ class ProductCollection extends ResourceCollection
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+
+        return [
+            'name' => $this->name,
+            'total_price' => round((1 - ( $this->discount / 100 ) ) *  $this->price),
+            'rating' => $this->reviews->count() > 0 ? round($this->reviews->sum('star')/$this->reviews->count()) : 'No Rating Yet',
+            'href' => [
+                'link' => route('products.show', $this->id),
+            ],
+        ];
     }
 }
